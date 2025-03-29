51b67134d493d817950a6a784778bae176ca4294
Ilia Prokhorov <demensdeum@gmail.com>
2025-03-29 23:12:45 +0400

    OpenCL test script file

--- src/opencl/test.sh
+++ src/opencl/test.sh
@@ -1,3 +1,32 @@
+#!/bin/bash
+
 make clean
 make
-./enpakk_gpu ../compressed.enpakk uncompressed.txt
+
+python3 ../enpakk_compress.py compress ../test.txt ../compressed.enpakk
+
+while true; do
+    ./enpakk_gpu ../compressed.enpakk uncompressed.txt
+
+    if cmp -s "uncompressed.txt" "../test.txt"; then
+        echo "Files match!"
+        break
+    else
+        echo "Content of ../test.txt:"
+        cat ../test.txt
+        echo ""
+
+        echo "Content of ../uncompressed.txt:"
+        cat ../uncompressed.txt
+        echo ""
+        
+        echo "Files don't match, retrying..."
+        echo -n "CRC32 uncompressed.txt: "
+        crc32 ../test.txt
+        echo -n "CRC32 compressed.enpakk: "
+        crc32 ../test.txt
+        echo ""
+
+        exit 0
+    fi
+done
--- src/test.sh
+++ src/test.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 python3 enpakk_compress.py compress test.txt compressed.enpakk
 #python3 enpakk_decompress_cpu.py compressed.enpakk uncompressed.txt
 
